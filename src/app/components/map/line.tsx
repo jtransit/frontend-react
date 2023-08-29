0477dbfbe7e4783121e1758151e30f17877e2e05
Ivan Apos <aposivanrodger@gmail.com>
2023-08-29 16:00:49 +0800

    Display all routes

--- src/app/components/map/line.tsx
+++ src/app/components/map/line.tsx
@@ -9,18 +9,22 @@ const colors = ['blue', 'red', 'green', 'purple', 'orange'];
 const Line = () => {
   const { routes } = useMapContext();
 
-  // TODO: WIP API routing
   const generateLine = () => {
     if (routes?.length > 0) {
-      // TODO: WIP types and routings
-      return (routes[0] as any).points.map((p: any, i: number) => {
-        return (
-          <Polyline
-            key={i}
-            pathOptions={{ color: colors[(i % colors.length) + 1], weight: 5 }}
-            positions={decode(p)}
-          />
-        );
+      return routes.map((r: Record<string, unknown>) => {
+        // TODO: WIP types and routings
+        return (r as any).points.map((p: any, i: number) => {
+          return (
+            <Polyline
+              key={i}
+              pathOptions={{
+                color: colors[(i % colors.length) + 1],
+                weight: 5,
+              }}
+              positions={decode(p)}
+            />
+          );
+        });
       });
     }
     return null;
