bda50eca4ebb4854a60687914396929c034f5fda
Ivan Apos <aposivanrodger@gmail.com>
2023-09-22 11:06:52 +0800

    Fixed merge conflict

--- src/app/components/map/component.tsx
+++ src/app/components/map/component.tsx
@@ -1,13 +1,24 @@
 'useClient';
 import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 import { MapContextProvider } from '@contexts/map-context';
 import { MapEvents } from '@components/map/events';
 import { ContextMenu } from '@components/menu/context-menu';
 import Line from '@components/map/line';
-import NavigationWrapper from '@components/menu/navigation-wrapper';
+import NavigationMenu from '@components/menu/navigation-menu';
+import Markers from './markers';
+
+L.Marker.prototype.options.icon = L.icon({
+  iconUrl: icon.src,
+  shadowUrl: iconShadow.src,
+  iconAnchor: [15, 40],
+  popupAnchor: [0, -51],
+});
 
 const MapComponent = () => {
   return (
@@ -24,7 +35,8 @@ const MapComponent = () => {
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
         <ContextMenu />
-        <NavigationWrapper />
+        <NavigationMenu />
+        <Markers />
         <Line />
         <MapEvents />
       </MapContextProvider>
